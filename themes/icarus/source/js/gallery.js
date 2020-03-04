256313ec68b643f20afa6c0d335d1e69bdf1a586
Kihoon Han <me@hoony9x.com>
2020-03-05 00:07:05 +0900

    사진 표시 관련 설정 변경

--- themes/icarus/source/js/gallery.js
+++ themes/icarus/source/js/gallery.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
         if ($('.justified-gallery > p > .gallery-item').length) {
             $('.justified-gallery > p > .gallery-item').unwrap();
         }
-        $('.justified-gallery').justifiedGallery();
+        $('.justified-gallery').justifiedGallery({
+            lastRow: 'justify'
+        });
     }
 });
